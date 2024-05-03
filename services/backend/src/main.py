5167501a7619ce8ddde69b7a14ebe6c63bc53c45
JordanTx <jordantx21@gmail.com>
2024-05-02 20:22:26 -0500

    ahora si se puede empezar

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from pathlib import Path
+import re
 from src.routes.user import user
+import mimetypes
+mimetypes.init()
 
 app = FastAPI()
 
@@ -22,16 +25,20 @@ try:
     index_path = build_dir / "index.html"
 
     # Serve assets files from the build directory
-    app.mount("/assets", StaticFiles(directory=build_dir / "assets"), name="assets")
+    app.mount("/static", StaticFiles(directory=build_dir, html=False), name="static")
 
     # Catch-all route for SPA
-    @app.get("/{catchall:path}")
+    @app.get("/{catchall:path}", response_class=HTMLResponse)
     async def serve_spa(catchall: str):
         # If the requested file exists, serve it, else serve index.html
+        mimetypes.add_type('application/javascript', '.js')
+        mimetypes.add_type('text/css', '.css')
+        mimetypes.add_type('image/svg+xml', '.svg')
         path = build_dir / catchall
         if path.is_file():
             return FileResponse(path)
         return FileResponse(index_path)
+
 
 except RuntimeError:
     # The build directory does not exist
