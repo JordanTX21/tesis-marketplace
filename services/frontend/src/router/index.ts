8a7970e6d9790d455312791f6713303059590ce5
JordanTX21 <jordantx21@gmail.com>
2024-05-13 21:45:01 -0500

    vista de home

--- services/frontend/src/router/index.ts
+++ services/frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MarketView from '../views/MarketView.vue'
+import ProfileView from '../views/ProfileView.vue'
 import AuthRoutes from '@/modules/auth/router'
 
 const router = createRouter({
